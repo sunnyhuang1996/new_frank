2879392f23b3d5393cc4dc51d1d517daadd2afc5
Fang Tiantian <g5fangti@cdf.toronto.edu>
2016-03-29 19:13:24 -0400

    add

--- gmmTrain.m
+++ gmmTrain.m
@@ -15,3 +15,102 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
 %                                          is a vector
 %                            gmm.cov     : DxDxM matrix of covariances. 
 %                                          (:,:,i) is for i^th mixture
+
+D = 14;
+gmms = {};
+speakers_list = dir(dir_train);
+speakers_list = speakers_list(3:length(speakers_list));
+for s_i = 1:length(speakers_list)
+    name = speakers_list(s_i).name;
+    gmms{s_i} = struct();
+    gmms{s_i}.name = name;
+
+    %get into dir of name
+    mfcc_files = dir([dir_train, '/', name, '/', '*.mfcc']);
+    %read data
+    data = [];
+    for f = 1:length(mfcc_files)
+        file = [dir_train, '/', name, '/', mfcc_files(f).name];
+        data = [data; textread(file)];
+    end
+    data = data(:, 1:D);
+    data = data.';   %D*t
+    
+    %initalize 
+    randConst = 1;
+    weight = randConst +rand(M,1);
+    weight = weight/sum(weight);
+
+    mu = zeros(D,M);
+    for q=1:M
+        mu(:,q) = data(:, ceil(2726/M*q));
+    end
+    vary = ones(D, M);
+
+    i = 0;
+    prev_L = -Inf;
+    improvement = Inf;
+    while i<=max_iter && improvement>=epsilon
+        [L, pm_x] = ComputeLikelihood(data, mu, vary, weight, M);
+        [weight, mu, vary] = UpdateParameters(data, pm_x);
+        improvement = L-prev_L;
+        i = i+1;
+    end
+    
+    gmms{s_i}.weights = weight;
+    gmms{s_i}.means = mu;
+    gmms{s_i}.cov = zeros(D, D, M);
+
+    for i=1:M
+        sigma = diag(vary(:, i));
+        gmms{s_i}.cov(:,:,i) = sigma;
+    end
+end
+
+
+function [L, pm_x] = ComputeLikelihood(data, mu, vary, weight, M)
+[d, t] = size(data);
+bm_x = zeros(M,t);
+nomi = zeros(M, t);
+deno = zeros(1, M);
+
+% calculate denominator
+for i=1:M
+    sigma = vary(:,i);
+    deno(i) = (2*pi)^(d/2) * (prod(sigma))^0.5;
+end
+
+%calculate p(x|z)
+for i = 1:M
+    for j=1:t
+        a = sum(((data(:,j)-mu(:,i)).^2)./vary(:,i));
+        nomi(i,j) = exp(-0.5 * a);
+    end
+    bm_x(i,:)=nomi(i,:)/deno(i);
+end
+
+wbm=zeros(M,t);
+for i=1:M
+    wbm(i,:) = weight(i) * bm_x(i,:);
+end
+
+%calculate p(z)
+pm_x = bsxfun(@rdivide, wbm, sum(wbm));
+
+% p(x) = sum p(z)p(x|z)
+px = bm_x .* pm_x;
+px = sum(px);
+
+logpx = log(px);
+L = sum(logpx);
+
+
+
+function [weight, mu, vary] = UpdateParameters(data, pm_x)
+data = data';
+T = size(data,1);
+common_sum = sum(pm_x,2)';
+weight = common_sum/T;
+mu = bsxfun(@rdivide, (pm_x * data)', common_sum);
+vary = bsxfun(@rdivide, (pm_x * (data.^2)).', common_sum) - mu.^2;
+        
